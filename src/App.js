fec158d048123e177da2a6571efb0df8005da27b
payalkherajani <payal.kherajani19@gmail.com>
2020-12-18 20:47:38 +0530

    Added-Functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [emoji,setEmoji] = useState("");
+  const [meaning,setMeaning] = useState("Translation will come here!")
+
+  const dic = {
+        "🍇":"grapes" ,
+        "🍋": "lemon" ,
+        "🥑": "avocado",
+        "🍄": "mushroom",
+        "🍕": "pizza",
+        "🍔": "Hamburger",
+        "🍩": "Doughnut",
+        "🦪": "Oyster"
+    }
+
+  const onChangeHandler = (e) => {
+   const inputEmoji = e.target.value;
+   setEmoji(inputEmoji);
+
+   if(inputEmoji in dic){
+    const dicOutput = dic[inputEmoji];
+    setMeaning(dicOutput)
+   }
+   else{
+   setMeaning("Sorry! We dont have this emoticon");
+
+   }
+  
+ 
+  }
+
+const dicArr = Object.keys(dic);
+const emojiHover = (emoji) => {
+ if(emoji in dic){
+   setMeaning(dic[emoji]);
+ }
+ else{
+   setMeaning("Sorry! We dont have this emoticon");
+ }
+;}
+
   return (
     <div className="App">
-      <h1>hello React</h1>
+      <h1>Food Emoticons</h1>
+      <div>
+        <input type="text" placeholder="Search your emoji here!" value={emoji} onChange={onChangeHandler}/>
+        <div>{meaning}</div>
+        <div>
+          {
+             dicArr.map(item => {
+                return <div key={item} onClick={() => emojiHover(item)}> {item} </div>
+             })
+          }
+          </div>
+      </div>
     </div>
   );
 }
